8589fba3a968119cff6d559e37b8e90529c488dd
Alex Kaufman <alexkaufman06@gmail.com>
2015-07-14 14:21:31 -0700

    Fix invert 100+% bug

--- js/controllers/sun.js
+++ js/controllers/sun.js
@@ -1,6 +1,6 @@
 sun.controller('SunCtrl',function($scope,$interval){
   $scope.sun = { sep: 80, inv: 5, sat: 70, hue: 0 };
-  
+
   var sunSat= $interval(function() {
     $scope.sun.sat += 1;
   },1);
@@ -10,6 +10,9 @@ sun.controller('SunCtrl',function($scope,$interval){
   },100)
 
   var sunInv= $interval(function() {
+    if ($scope.sun.inv === 100) {
+      $scope.sun.inv = 1;
+    }
     $scope.sun.inv += 1;
   }, 1000)
 
